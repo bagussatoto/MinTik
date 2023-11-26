3257fe4a85ccb608b83d33b19c9a9bb51bb165f2
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-11-26 09:37:51 +0700

    mai

--- app.py
+++ app.py
@@ -20,23 +20,23 @@ words = pickle.load(open('model/texts.pkl','rb'))
 classes = pickle.load(open('model/labels.pkl','rb'))
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
+    tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
+    stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+    tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
+    bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)  
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s: 
-                # assign 1 if current word is in the vocabulary position
+                assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
@@ -55,14 +55,14 @@ def predict_class(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
-# def getResponse(ints, intents_json):
-#     tag = ints[0]['intent']
-#     list_of_intents = intents_json['intents']
-#     for i in list_of_intents:
-#         if(i['tag']== tag):
-#             result = random.choice(i['responses'])
-#             break
-#     return result
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
 
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
